816e65238061dcded259bba94abd2c8d58fd0316
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-01-25 12:58:31 -0600

    setup port, server, and rejection catching

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const app = express();
 
+const PORT = process.env.PORT || 5001;
+const server = app.listen(PORT, () => { console.log(`Server is listening on PORT ${PORT}`); });
+
+process.on('unhandledRejection', (error) => {
+    console.log(`Error: ${error.message}`);
+    server.close(() => {process.exit(1)}); // gracefully terminates the server on failure synchronously - 1 indicates server failure
+});
