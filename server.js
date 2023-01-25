4d9ff0d7f6a27b42d6de9ba38e1f20433d66bc29
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-01-25 13:40:04 -0600

    connected database to server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const app = express();
+const connectDB = require('./config/db');
+
+connectDB(); // it is important to connect to database BEFORE initializing Express
+const app = express(); // initalizing Express
 
 const PORT = process.env.PORT || 5001;
 const server = app.listen(PORT, () => { console.log(`Server is listening on PORT ${PORT}`); });
