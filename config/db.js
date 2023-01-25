d92ca4402593c9513646fd5d8e25b263eac08b3e
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-01-25 13:56:56 -0600

    reworked .env variables to root folder, changed PORT, updated .gitignore

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+    mongoose.set('strictQuery', true); // fixes deprecation warning
     const conn = await mongoose.connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
-
+    
     console.log(`MongoDB Connected: ${conn.connection.host}`);
 }
 
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const dotenv = require('dotenv');
+require('dotenv').config();
 const connectDB = require('./config/db');
 
 connectDB(); // it is important to connect to database BEFORE initializing Express
 const app = express(); // initalizing Express
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5006;
 const server = app.listen(PORT, () => { console.log(`Server is listening on PORT ${PORT}`); });
 
 process.on('unhandledRejection', (error) => {
