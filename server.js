9b479c898ea74ca202a7af220e17609741ff153c
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-01-25 16:34:20 -0600

    imported logger and finalized logger.js code

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config/config.env' });
 const connectDB = require('./config/db');
+const logger = require('./utils/logger');
 
 connectDB(); // it is important to connect to database BEFORE initializing Express
 const app = express(); // initalizing Express
 
 const PORT = process.env.PORT || 5006;
+app.use(logger);
 const server = app.listen(PORT, () => { console.log(`Server is listening on PORT ${PORT}`); });
 
 process.on('unhandledRejection', (error) => {
--- utils/logger.js
+++ utils/logger.js
@@ -1,3 +1,6 @@
 const logger = (req, res, next) => {
-    console.log(`Request Method: ${req.method} + Request Protocol: ${req.protocol} ://${req.get('host')} ${req.originalURL}`);
+    console.log(`Request Method: ${req.method} / Request Protocol: ${req.protocol} / Get Host: ${req.get('host')} / URL: ${req.originalURL}`);
+    next();
 };
+
+module.exports = logger;
