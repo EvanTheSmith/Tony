81b6487d82d5db9c81876db251ef7a10784339b9
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-01-27 13:39:58 -0600

    fixed env file having semicolons, added comment, successfully made postman post request

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config/config.env' });
 const connectDB = require('./config/db');
+const bodyParser = require('body-parser');
 const logger = require('./utils/logger');
 const user = require('./routes/user'); // router import
 
 connectDB(); // it is important to connect to database BEFORE initializing Express
 const app = express(); // initalizing Express
+app.use(bodyParser.json());
 
-const PORT = 5013 // process.env.PORT || 5006;
+const PORT = process.env.PORT || 5020;
 app.use(logger);
 app.use('/user', user); // connected to routes/user import above
 const server = app.listen(PORT, () => { console.log(`Server is listening on PORT ${PORT}`); });
