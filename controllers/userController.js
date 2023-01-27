ebf7b7702afd493efecfc08a4245972db23bffc8
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-01-27 12:30:48 -0600

    built createUser mongoose route

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,9 +11,10 @@ const getUsers = async (req, res, next) => {
 
 const createUser = async (req, res, next) => {
     try {
-
+        const result = await User.create(req.body);
+        res.status(200).setHeader('Content-Type', 'application/json').json(result);
     } catch (error) {
-
+        throw new Error(`Error creating a user: ${error.message}`)
     }
 }
 
