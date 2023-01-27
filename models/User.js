a2d601ff53fd9eb824b0079fa74b6fe709abb743
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-01-27 13:12:21 -0600

    bug testing server not working in browser / temporarily locking PORT for bug fixing

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema(); // instantiates Schema as a class, using the mongoose library
+const Schema = mongoose.Schema; // instantiates Schema as a class, using the mongoose library
 
 const UserSchema = new Schema({
     userName: {
--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config/config.env' });
 const connectDB = require('./config/db');
 const logger = require('./utils/logger');
+const user = require('./routes/user'); // router import
 
 connectDB(); // it is important to connect to database BEFORE initializing Express
 const app = express(); // initalizing Express
 
-const PORT = process.env.PORT || 5006;
+const PORT = 5013 // process.env.PORT || 5006;
 app.use(logger);
+app.use('/user', user); // connected to routes/user import above
 const server = app.listen(PORT, () => { console.log(`Server is listening on PORT ${PORT}`); });
 
 process.on('unhandledRejection', (error) => {
