4aea7eed0fec9d7a0a713a4fc40f98c61fc547fc
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-01-27 13:42:14 -0600

    replaced body-parser library with built-in express json() method

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config/config.env' });
 const connectDB = require('./config/db');
-const bodyParser = require('body-parser');
 const logger = require('./utils/logger');
 const user = require('./routes/user'); // router import
 
 connectDB(); // it is important to connect to database BEFORE initializing Express
 const app = express(); // initalizing Express
-app.use(bodyParser.json());
+app.use(express.json()); // parse JSON for Postman
 
 const PORT = process.env.PORT || 5020;
 app.use(logger);
